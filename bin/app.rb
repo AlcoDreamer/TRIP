b21e46ec426711ba09b458e9549ce783871eb962
AlcoDreamer <pyshbol@gmail.com>
2019-03-04 23:42:05 +0300

    extension

--- bin/app.rb
+++ bin/app.rb
@@ -18,8 +18,8 @@ set :static, true
 set :views, "views"
 set :public_directory, "public"
 
-class ImageUploader < CarrierWave::Uploader::Base
-  include CarrierWave::MiniMagick
+class ImagesUploader < CarrierWave::Uploader::Base
+  #include CarrierWave::MiniMagick
 
   def cache_dir
     "../tmp/uploads"
@@ -57,7 +57,7 @@ end
 
 class Mark < ActiveRecord::Base
   #validates :car_number, presence: true, length: { maximum: 15 }
-  mount_uploader :image, ImageUploader
+  mount_uploader :image, ImagesUploader
 end
 
 helpers do
@@ -91,17 +91,6 @@ post "/marks" do
   #mark = params[:mark]
   @mark.save
   redirect "marks/#{@mark.id}"
-=begin    
-  @mark = Mark.new
-  @mark.car_number = params[:mark][:car_number]
-  
-  if @mark.save
-    redirect "marks/#{@mark.id}", :notice => 'Классно! Пост добавлен!'
-  else
-    redirect "marks/new", :error => 'Ты напортачил, попробуй еще!'
-  end
-=end
-
 end
 
 #Get one mark by ID
@@ -126,81 +115,3 @@ put "/marks/:id" do
   @mark.update(params[:mark])
   redirect "/marks/#{@mark.id}"
 end
-
-=begin
-
-class Mark
-
-  field :car_number, type: String
-  field :tag, type: String
-  field :ssn
-  
-  validates :car_number, presence: true
-  index({ ssn: 1 }, { unique: true, name: "ssn_index" })
-end
-
-# Корень
-get '/' do
-  puts 'Yasya forever!'
-  erb :"index"
-end
-
-namespace '/v1' do
-
-  # Список всех публикаций
-  get '/marks' do
-    content_type 'application/json'
-    marks = Mark.all
-    return marks.to_json
-  end
-
-  get '/clean' do
-    puts 'clean!'
-    Mark.delete_all
-  end
-
-  # Форма для добавления публикации
-  get '/marks/new' do
-    #erb :"/marks/new"
-  end
-  
-  # Одна публикация
-  get '/marks/:id' do
-
-  end
-  
-  # Сохранение публикации
-  post '/marks' do
-    digests = self.request.body.read
-    data = JSON.parse( digests )
-
-    puts "\nI got request: \"#{digests}\""
-    puts "\nI got JSON: \"#{data}\""
-    puts "\nCar number: \"#{data['mark']['car_number']}\"\n\n"
-
-    @mark = Mark.new(car_number: data['mark']['car_number'], tag: data['mark']['tag'])
-    @mark.save
-  end
-
-  # Обновление отредактированной публикации
-  put '/marks/:id' do
-    
-  end
-
-  # Форма для редактировани публикации
-  get '/marks/:id/edit' do
-    
-  end
-
-  # Стриница с предупреждением об удалении публикации
-  get '/marks/:id/delete' do
-    
-  end
-
-  # Удаление публикации
-  delete '/marks/:id' do
-
-  end
-end
-
-=end
